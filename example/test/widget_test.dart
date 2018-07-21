7d7bad499af5bdc87baa44719f6de3795d55c785
leocavalcante <lc@leocavalcante.com>
2018-07-20 21:47:14 -0300

    Fix subpackage import

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -7,7 +7,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:page_view_indicator_example/main.dart';
+import '../lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
